6ee7151c54b962cbec4344b8a16aaa2d1127fbff
vleng-gangkangkung <90861876+vleng-gangkangkung@users.noreply.github.com>
2022-02-05 02:21:37 +0000

    formatted Forecast Dates

--- src/components/ForecastDay.js
+++ src/components/ForecastDay.js
@@ -4,29 +4,40 @@ import Icon from "../Icon";
 import "../styles.css";
 import "../Forecast.css";
 export default function ForecastDay(props) {
+  let maxTemperature = Math.round(props.data.temp.max);
+  let minTemperature = Math.round(props.data.temp.min);
+
+  let date = new Date(props.data.dt * 1000);
+  let day = date.getDay();
+  let month = date.getMonth();
+  let formattedDate = date.getDate();
+
+  if (month <= 9) {
+    month = `0${month}`;
+  }
+
+  if (formattedDate <= 9) {
+    formattedDate = `0${formattedDate}`;
+  }
+
+  let daysMapped = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  console.log(date);
+
   return (
     <div className="col forecast-dayDate">
       <div className="border-dates-forecast ">
-        <div className="day">{props.day}</div>
-        <div>{props.data.dt}</div>
+        <div className="day">{daysMapped[day]}</div>
+        <div>
+          {formattedDate}/{month}
+        </div>
       </div>
       <div className="forecast-icon pe-4">
         <Icon size={75} weatherIcon={props.data.weather[0].icon} />
       </div>
       <div className="forecastTemperature" id="forecast-temp-max">
-        {Math.round(props.data.temp.max)}&#176;&nbsp;
-        <span className="forecastTemperatureMin">
-          {Math.round(props.data.temp.min)}&#176;
-        </span>
+        {maxTemperature}&#176;&nbsp;
+        <span className="forecastTemperatureMin">{minTemperature}&#176;</span>
       </div>
     </div>
-
-    // <ForecastDay
-    //         day="THU"
-    //         date={forecast[0].dt}
-    //         maxTemp={forecast[0].temp.max}
-    //         minTemp={forecast[0].temp.min}
-    //         icon={forecast[0].weather[0].icon}
-    //       />
   );
 }
