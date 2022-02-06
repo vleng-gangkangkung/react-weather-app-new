643b8ec424ce2d43f39f33783bed0abe95ea4883
vleng-gangkangkung <90861876+vleng-gangkangkung@users.noreply.github.com>
2022-02-06 00:30:38 +0000

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -7,10 +7,15 @@ export default function Footer() {
     <div className="Footer">
       <small>
         Coded and designed by
-        <a href="https://vleng.co.uk">&nbsp;Vanessa Leng&nbsp;</a>|
+        <a href="https://vleng.co.uk" target="_blank" rel="noopener noreferrer">
+          &nbsp;Vanessa Leng&nbsp;
+        </a>
+        |
         <a
           href="https://github.com/vleng-gangkangkung/react-weather-app-new"
+          rel="noopener noreferrer"
           title="View on Github"
+          target="_blank"
         >
           &nbsp;Open Source Code
         </a>
